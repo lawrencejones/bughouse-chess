d38a09e98f4fa5ae916a6ae917311fdbd6d93a99
Tomas Virgl <tomas@virgl.net>
2014-05-26 20:07:18 +0100

    small fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
   	if @user.save
   		sign_in @user
-  		flash[:success] = "Welcome to the Sample App!"
+  		flash[:success] = "Welcome to the Bughouse Chess App!"
   		redirect_to @user
   	else
   		render 'new'
