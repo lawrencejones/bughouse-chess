639193a98b704dd1b494a96a40fbc82093a2092c
YuryB <yurybelevskiy@gmail.com>
2014-05-28 00:21:34 +0100

    Finalizing emailing system

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ActionMailer::Base
-  default from: "bughouse-chess-people@example.com"
+  default from: "support@bughouse-chess.com"
   
   def send_signup_email(user)
     @user = user
-    mail( to: @user.email, subject: "Thanks for signing up with Bughouse Chess Online")
+    mail( to: @user.email, subject: "Thanks for signing up")
   end
 end
